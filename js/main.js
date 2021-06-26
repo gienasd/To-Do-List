c9c100e2ad62c7c334b4f8ab9ae91548916fcc98
arctindev <arctindev@gmail.com>
2021-06-26 13:13:00 +0200

    Styled with SCSS, Added BEM methodology for html elements,js architecture changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('eloszka');
+import Task from './task_utils.mjs';
+
+const addTaskInput = document.querySelector('.add-task-block__input');
+const addTaskButton = document.querySelector('.add-task-block__btn');
+const taskList = document.querySelector('.task-list');
+
+let taskId = 0;
+let task = [];
+
+const addTask = () => {
+
+    task[taskId] = new Task(taskId,addTaskInput.value,taskList);
+    addTaskInput.value = '';
+    taskId++;
+    
+    console.log(task);
+}
+
+
+addTaskButton.addEventListener('click',addTask)
+
